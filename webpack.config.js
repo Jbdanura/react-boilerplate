101e1c1f8178aca5722c59a5bfee8a9b40597e33
jotabe <danurabe@gmail.com>
2022-03-27 16:21:46 -0300

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { NONAME } = require('dns');
 
 module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
